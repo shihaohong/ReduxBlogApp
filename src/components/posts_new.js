f067e61003352553abcbbab580bb7dd1bc121986
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-14 19:59:56 -0700

    added new posts page

---
+++ src/components/posts_new.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    )
+  }
+}
+
+function validate(values) {
+  // console.log(values) -> { title:'asdf, categories: 'asdf', content: 'asdf'}
+  const errors = {};
+
+  // Validate inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  } 
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, reduxForm assumes form is invalid
+  return errors;
+}
+
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm' // form value HAS TO BE UNIQUE, but
+})(PostsNew);          // it can be whatever you want it to be
