3a28285921b2167e64aa6483ea7704a01d360f6e
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-15 11:23:15 -0700

    show posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
   renderField(field) {
+    const { meta: { touched, error } } = field;
+
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
-  render() {
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() { 
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title For Post"
           name="title"
@@ -36,6 +52,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -66,4 +83,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm' // form value HAS TO BE UNIQUE, but
-})(PostsNew);          // it can be whatever you want it to be
+})(
+  connect(null, { createPost })(PostsNew)
+);          // it can be whatever you want it to be
